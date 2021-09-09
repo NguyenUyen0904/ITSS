1ccb7952d51786f799e78781da7a343b7e711d71
Nguyễn Uyên <55393354+NguyenUyen0904@users.noreply.github.com>
2021-09-09 11:14:54 +0700

    Bài 2
    làm bài 2 trong bài kiểm tra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStudent = (initStudents) => {
+	const [students, setStudents] = useState(initStudents);
+
+	const removeStudent = (name) => {
+		const index = students.indexOf(name);
+		if (index !== -1) {
+			students.splice(index, 1);
+			setStudents([...students]);
+		}
+	};
+
+	return [students, removeStudent];
+
+}
+
+const App = () => {
+	const [students, removeStudent] = useStudent(['Huyen', 'Hoa', 'Hung', 'Long']);
+	const [value, setValue] = useState('');
+
+	const handleChange = (event) => {
+		setValue(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		removeStudent(value);
+	};
+
+	return (
+		<div>
+			<div>学生一覧： {['Huyen', 'Hoa', 'Hung', 'Long'].join(', ')}</div>
+      <div></div>
+			<div>削除値名前を入力してください。  </div>
+			<input value={value} onChange={handleChange} /><br/>
+			<button onClick={handleSubmit}>確定</button>
+			<div>削除する名前： {value}</div>
+			<div>新しい一覧：{students.join(', ')}</div>
+		</div>
+	);
 }
 
 export default App;
